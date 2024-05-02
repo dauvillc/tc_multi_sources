0bce12d70b57160ac09e6b4ef152145bfcb600ce
Clément Dauvilliers <clement.dauvilliers@inria.fr>
2024-05-02 17:10:26 +0200

    Time measurement of data loading

--- tests/single_2d_source_dataset.py
+++ tests/single_2d_source_dataset.py
@@ -1,5 +1,8 @@
 """Tests the Single2DSourceDataset class."""
 import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from time import time
+from tqdm import trange
 from multi_sources.data_processing.utils import read_source_file
 from multi_sources.data_processing.single_2d_source_dataset import Single2DSourceDataset
 
@@ -17,9 +20,17 @@ def main():
     # Plot each variable
     for i, var in enumerate(variables):
         plt.subplot(1, len(variables), i + 1)
-        plt.imshow(sample[var])
+        plt.imshow(sample[i])
         plt.title(var)
     plt.savefig('tests/figures/single_2d_source_dataset.png')
+
+    # Now, create a DataLoader from the dataset, and measure the time needed
+    # to iterate over all samples
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=9)
+    start = time()
+    for i, batch in zip(trange(len(dataset) // 64), dataloader):
+        pass
+    print(f"Time to iterate over all samples: {time() - start:.2f} seconds")
 
 
 if __name__ == "__main__":
