Fixed loss backprop in autoencoding setting

multi_sources/data_processing/multi_source_dataset.py
@@ -11,7 +11,7 @@ class MultiSourceDataset(torch.utils.data.Dataset):
 
     The dataset yields maps {source_name: (S, DT, C, D, V)} where:
     - S is the source index.
-    - DT is the time delta between the reference time and the time of the element, in hours.
+    - DT is the ratio of the time difference between the element and the synoptic time to the maximum time.
     - C is a tensor of shape (3, H, W) containing the coordinates (lat, lon) of each pixel,
         and the land-sea mask.
     - D is a tensor of shape (H, W) containing the distance at each pixel to the center of the storm.
@@ -169,7 +169,9 @@ class MultiSourceDataset(torch.utils.data.Dataset):
                 time = df["time"].iloc[0]
                 dt = time - syn_time
                 # Create the S and DT tensors
-                dt_tensor = torch.tensor(dt.total_seconds() / 3600, dtype=source_tensor.dtype)
+                dt_tensor = torch.tensor(
+                    dt.total_seconds() / self.dt_max.total_seconds(), dtype=torch.float32
+                )
                 # Load the npy file containing the data for the given storm, time, and source
                 filepath = self.get_data_filepath(season, basin, sid, time, source_name)
                 tensor = torch.from_numpy(np.load(filepath)).to(torch.float32)

multi_sources/models/unet.py
@@ -24,7 +24,7 @@ class UNet(nn.Module):
     fed to the UNet.
     """
 
-    def __init__(self, n_variables, n_blocks, base_filters, kernel_size):
+    def __init__(self, n_variables, n_blocks=4, base_filters=32, kernel_size=3):
         """
         Args:
             n_variables (dict): dict {source: n_variables} containing the number of variables for each source.
@@ -58,16 +58,20 @@ class UNet(nn.Module):
                         nn.Conv2d(
                             in_channels, out_channels, kernel_size, padding=kernel_size // 2
                         ),
-                        nn.GELU(),
+                        nn.SiLU(),
                         nn.BatchNorm2d(out_channels),
                         nn.Conv2d(
                             out_channels, out_channels, kernel_size, padding=kernel_size // 2
                         ),
-                        nn.GELU(),
+                        nn.SiLU(),
                         nn.BatchNorm2d(out_channels),
                     ),
                     # Apply a conv layer with stride 2 to downsample the input
-                    nn.Conv2d(out_channels, out_channels * 2, 2, stride=2, padding=0),
+                    nn.Sequential(
+                        nn.Conv2d(out_channels, out_channels * 2, 2, stride=2, padding=0),
+                        nn.SiLU(),
+                        nn.BatchNorm2d(out_channels * 2),
+                    ),
                 ]
             )
             in_channels = out_channels * 2
@@ -79,13 +83,17 @@ class UNet(nn.Module):
             decoder_block = nn.ModuleList(
                 [
                     # Upsample the input
-                    nn.ConvTranspose2d(
-                        in_channels,
-                        in_channels,
-                        2,
-                        stride=2,
+                    nn.Sequential(
+                        nn.ConvTranspose2d(
+                            in_channels,
+                            in_channels,
+                            2,
+                            stride=2,
+                        ),
+                        nn.SiLU(),
+                        nn.BatchNorm2d(in_channels),
                     ),
-                    # Apply two conv layers with GELU activation and batch normalization
+                    # Apply two conv layers with SiLU activation and batch normalization
                     nn.Sequential(
                         nn.Conv2d(
                             in_channels + in_channels // 2,
@@ -93,31 +101,56 @@ class UNet(nn.Module):
                             kernel_size,
                             padding=kernel_size // 2,
                         ),
-                        nn.GELU(),
+                        nn.SiLU(),
                         nn.BatchNorm2d(out_channels),
                         nn.Conv2d(
                             out_channels, out_channels, kernel_size, padding=kernel_size // 2
                         ),
-                        nn.GELU(),
+                        nn.SiLU(),
                         nn.BatchNorm2d(out_channels),
                     ),
                 ]
             )
             in_channels = in_channels // 2
             self.decoder.append(decoder_block)
+        # Output conv layer
+        self.output_layers = nn.ModuleList(
+            [
+                nn.Conv2d(
+                    self.output_channels,
+                    self.output_channels,
+                    kernel_size,
+                    padding=kernel_size // 2,
+                ),
+                nn.SiLU(),
+                nn.BatchNorm2d(self.output_channels),
+                nn.Conv2d(
+                    self.output_channels,
+                    self.output_channels,
+                    kernel_size,
+                    padding=kernel_size // 2,
+                ),
+            ]
+        )
 
-    def forward(self, x):
+    def forward(self, batch):
         """Forward pass of the model.
         Args:
             x (dict): dict {source: S, DT, C, V} containing the input tensors.
         Returns:
             y (dict): dict {source: V'} containing the output tensors.
         """
+        # Check that the number of sources is correct
+        if len(batch) != self.n_sources:
+            raise ValueError(f"Expected {self.n_sources} sources, got {len(batch)}.")
+        # Transform the input dict into a list of tensors, ordered by source index
+        x = {int(s[0].item()): (s, dt, c, v) for s, dt, c, v in batch.values()}
+        x = [x[i] for i in range(self.n_sources)]
         # For each source, pad C and V to the next multiple of 2^n_blocks
         # Then project S and DT into a tensor of shape (batch_size, 2, H, W)
         # - First, browse all sources to find their sizes
         widths, heights = [], []
-        for source_name, (_, _, _, v) in x.items():
+        for _, _, _, v in x:
             h, w = v.shape[-2:]
             heights.append(h)
             widths.append(w)
@@ -126,25 +159,21 @@ class UNet(nn.Module):
         h = (max(heights) + power - 1) // power * power
         w = (max(widths) + power - 1) // power * power
         # - Browse all sources to pad C and V to the computed size
-        st, original_sizes = {}, {}
-        for source_name, (s, dt, c, v) in x.items():
+        st, original_sizes = [], []
+        for k, (s, dt, c, v) in enumerate(x):
             # Pad C and V
-            original_sizes[source_name] = v.shape[-2:]
+            original_sizes.append(v.shape[-2:])
             c = F.pad(c, (0, w - c.shape[-1], 0, h - c.shape[-2]))
             v = F.pad(v, (0, w - v.shape[-1], 0, h - v.shape[-2]))
-            x[source_name] = (s, dt, c, v)
+            x[k] = (s, dt, c, v)
             # Project S and DT
             sdt = torch.stack((s, dt), dim=1)
             sdt = sdt.unsqueeze(-1).unsqueeze(-1).expand(-1, -1, h, w)
-            st[int(s[0].item())] = sdt
-        # Order the sources by index
-        st = [st[i] for i in range(self.n_sources)]
+            st.append(sdt)
         # For each source, concat SDT, C and V along the channel dimension
-        inputs = {}
-        for s, dt, c, v in x.values():
-            s = int(s[0].item())
-            inputs[s] = torch.cat((st[s], c, v), dim=1)
-        inputs = [inputs[i] for i in range(self.n_sources)]
+        inputs = []
+        for k, (s, dt, c, v) in enumerate(x):
+            inputs.append(torch.cat((st[k], c, v), dim=1))
         # Concatenate the inputs along the channel dimension
         x = torch.cat(inputs, dim=1)
         # Apply the encoder
@@ -158,11 +187,14 @@ class UNet(nn.Module):
             x = upsample(x)
             x = torch.cat((x, skips[-i - 1]), dim=1)
             x = conv(x)
+        # Apply the output layers
+        for layer in self.output_layers:
+            x = layer(x)
         # Split the output by source, and remove the padding
-        y = {}
-        for source, n in self.n_variables.items():
-            pred = x[:, :n]
-            x = x[:, n:]
-            h, w = original_sizes[source]
-            y[source] = pred[..., :h, :w]
+        y, n = {}, 0
+        for k, source_name in enumerate(batch.keys()):
+            h, w = original_sizes[k]
+            n_vars = self.n_variables[source_name]
+            y[source_name] = x[:, n : n + n_vars, :h, :w]
+            n += n_vars
         return y

multi_sources/structure/mae.py
@@ -1,6 +1,8 @@
 """Implements the MultisourceMaskedAutoencoder class"""
+
 import pytorch_lightning as pl
 import torch
+from multi_sources.utils.scheduler import CosineAnnealingWarmupRestarts
 
 
 class MultisourceMaskedAutoencoder(pl.LightningModule):
@@ -20,13 +22,16 @@ class MultisourceMaskedAutoencoder(pl.LightningModule):
     Attributes:
         model (torch.nn.Module): The model to wrap.
     """
-    def __init__(self, model):
+
+    def __init__(self, model, lr_scheduler_kwargs):
         """
         Args:
             model (torch.nn.Module): The model to wrap.
+            lr_scheduler_kwargs (dict): The keyword arguments to pass to the learning rate scheduler.
         """
         super().__init__()
         self.model = model
+        self.lr_scheduler_kwargs = lr_scheduler_kwargs
 
     def loss_fn(self, y_pred, y_true, masked_source_name):
         """Computes the reconstruction loss over the masked source.
@@ -49,10 +54,18 @@ class MultisourceMaskedAutoencoder(pl.LightningModule):
         # Compute the MSE loss element-wise
         loss = (y_pred[masked_source_name] - v) ** 2
         # Mask the loss for pixels outside the storm radius
-        loss = loss * (d.unsqueeze(1) < 1100)
-        # Set the loss to zero for NaN values in V
-        loss[torch.isnan(v)] = 0
+        # loss = loss * (d.unsqueeze(1) < 1100)
         return loss.mean()
+
+    def forward_and_compute_loss(self, x):
+        """Factorization of the forward pass and loss computation.
+        See training_step() for arguments and return values.
+        """
+        # Preprocess the input
+        x = self.preproc_input(x)
+        masked_x, masked_source_name = self.mask(x)
+        pred = self.forward(x)
+        return self.loss_fn(pred, x, masked_source_name)
 
     def training_step(self, batch, batch_idx):
         """Defines a training step for the model.
@@ -64,11 +77,8 @@ class MultisourceMaskedAutoencoder(pl.LightningModule):
         Returns:
             torch.Tensor: The loss of the model.
         """
-        # Randomly select a source and mask its values
-        masked_batch, masked_source_name = self.mask(batch)
-        pred = self.forward(masked_batch)
-        loss = self.loss_fn(pred, batch, masked_source_name)
-        self.log('train_loss', loss, prog_bar=True)
+        loss = self.forward_and_compute_loss(batch)
+        self.log("train_loss", loss, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -81,13 +91,12 @@ class MultisourceMaskedAutoencoder(pl.LightningModule):
         Returns:
             torch.Tensor: The loss of the model.
         """
-        masked_batch, masked_source_name = self.mask(batch)
-        pred = self.forward(masked_batch)
-        loss = self.loss_fn(pred, batch, masked_source_name)
-        self.log('val_loss', loss, prog_bar=True)
+        loss = self.forward_and_compute_loss(batch)
+        self.log("val_loss", loss, prog_bar=True)
         return loss
 
-    def forward(self, x):
+    def preproc_input(self, x):
+        """Fills NaN values in the input tensors with zeros, and normalizes the coordinates."""
         # x is a map {source_name: S, DT, C, D, V}
         # - Fill NaN values (masked / missing) in DT, C and V with zeros
         #   (which is also the mean value of the data after normalization)
@@ -99,12 +108,21 @@ class MultisourceMaskedAutoencoder(pl.LightningModule):
             dt = torch.nan_to_num(dt, nan=0)
             c = torch.nan_to_num(c, nan=0)
             v = torch.nan_to_num(v, nan=0)
-            input_[source] = (s, dt, c, v)
+            # Normalize the lat/lon coordinates
+            # Note: the lon is in the range [0, 360]
+            c[:, 0] = c[:, 0] / 90
+            c[:, 1] = (c[:, 1] - 180) / 180
+            input_[source] = (s, dt, c, d, v)
+        return input_
+
+    def forward(self, x):
+        # Remove the "distance to center" tensor from the input
+        input_ = {source: (s, dt, c, v) for source, (s, dt, c, _, v) in x.items()}
         return self.model(input_)
 
     def mask(self, x):
         """Given a training batch, randomly selects a source and masks all of its values.
-        
+
         Args:
             x (dict): The input batch. See the class description for the expected format.
 
@@ -112,22 +130,14 @@ class MultisourceMaskedAutoencoder(pl.LightningModule):
             masked_x (dict): The masked input batch.
             masked_source_name (str): The name of the source that was masked.
         """
-        # Select a source to mask, until finding a source whose values are not all NaN
-        source_name = None
-        perm = torch.randperm(len(x))
-        keys = list(x.keys())
-        for sname in [keys[i] for i in perm]:
-            _, _, _, _, v = x[sname]
-            if not torch.isnan(v).all():
-                source_name = sname
-                break
-        if source_name is None:
-            raise ValueError('All sources have NaN values, cannot use any of them as training target.')
+        # For now, just select the first source
+        source_name = list(x.keys())[0]
         masked_x = {}
         for source, (s, dt, c, d, v) in x.items():
+            continue
             if source == source_name:
                 # Mask the values
-                v = torch.full_like(v, float('nan')).to(v.dtype)
+                v = torch.full_like(v, float("nan")).to(v.dtype)
             masked_x[source] = (s, dt, c, d, v)
         return masked_x, source_name
 
@@ -137,8 +147,11 @@ class MultisourceMaskedAutoencoder(pl.LightningModule):
         The learning rate goes through a linear warmup phase, then follows a cosine
         annealing schedule.
         """
-        optimizer = torch.optim.AdamW(self.parameters(), lr=1e-6)
-        scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=1e-3, steps_per_epoch=100, epochs=10)
+        optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-4)
+        scheduler = CosineAnnealingWarmupRestarts(
+            optimizer,
+            **self.lr_scheduler_kwargs,
+        )
         return [optimizer], [scheduler]
 
     def __call__(self, x):