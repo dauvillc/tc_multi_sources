Major fixes on masking process

multi_sources/data_processing/multi_source_dataset.py
@@ -8,21 +8,20 @@ from pathlib import Path
 
 class MultiSourceDataset(torch.utils.data.Dataset):
     """A dataset that yields elements from multiple sources.
-
-    The dataset yields maps {source_name: (S, DT, C, D, V)} where:
-    - S is the source index.
-    - DT is the ratio of the time difference between the element and the synoptic time
-        to the maximum time.
-    - C is a tensor of shape (3, H, W) containing the coordinates (lat, lon) of each pixel,
-        and the land-sea mask.
-    - D is a tensor of shape (H, W) containing the distance at each pixel
-        to the center of the storm.
-    - V is a tensor of shape (channels, H, W) containing the values of each pixel.
-    If the element is not available for a source, DT, C, D, and V are filled with NaNs.
+    A sample from the dataset is a dict {source_name: (A, S, DT, C, D, V)}, where:
+    - A is a scalar tensor of shape (1,) containing 1 if the element is available and 0 otherwise.
+    - S is a scalar tensor of shape (1,) containing the index of the source.
+    - DT is a scalar tensor of shape (1,) containing the time delta between the synoptic time
+      and the element's time, normalized by dt_max.
+    - C is a tensor of shape (3, H, W) containing the latitude, longitude, and land-sea mask.
+    - D is a tensor of shape (H, W) containing the distance to the center of the storm.
+    - V is a tensor of shape (n_variables, H, W) containing the variables for the source.
 
     Each element is associated with a storm and a synoptic time. For a given storm S and time t0,
     the dataset returns the element from each source that is closest
     to t0 and between t0 and t0 - dt_max.
+    If for a given source, no element is available for the storm S and time t0, the element is
+    filled with NaNs.
     """
 
     def __init__(
@@ -140,8 +139,8 @@ class MultiSourceDataset(torch.utils.data.Dataset):
             idx (int): The index of the element to retrieve.
 
         Returns:
-            dict: A dictionary {source_name: (S, DT, C, D, V)}. If the element is not available
-                for a source, DT, C, D, and V are filled with NaNs.
+            sample (multi_sources.data_processing.multi_source_batch.MultiSourceBatch): The element
+                at the given index.
         """
         sample = self.samples_df.iloc[idx]
         sid = sample["sid"]
@@ -158,9 +157,11 @@ class MultiSourceDataset(torch.utils.data.Dataset):
             if len(df) == 0 or syn_time - df["time"].iloc[0] > self.dt_max:
                 # No element available for this source
                 # Create a tensor of the appropriate shape filled with NaNs
+                avail_tensor = torch.tensor(0, dtype=torch.float32)
                 source_shape = self.source_shapes[source_name]
                 source_channels = self.get_n_variables()[source_name]
                 output[source_name] = (
+                    avail_tensor,  # A
                     source_tensor,  # S
                     torch.tensor(float("nan"), dtype=torch.float32),  # DT
                     torch.full(
@@ -176,7 +177,7 @@ class MultiSourceDataset(torch.utils.data.Dataset):
             else:
                 time = df["time"].iloc[0]
                 dt = time - syn_time
-                # Create the S and DT tensors
+                avail_tensor = torch.tensor(1, dtype=torch.float32)
                 dt_tensor = torch.tensor(
                     dt.total_seconds() / self.dt_max.total_seconds(), dtype=torch.float32
                 )
@@ -203,8 +204,7 @@ class MultiSourceDataset(torch.utils.data.Dataset):
                 data_vars = df["data_vars"].iloc[0]
                 var_indices = [4 + data_vars.index(var) for var in selected_vars]
                 V = tensor[var_indices]
-                output[source_name] = (source_tensor, dt_tensor, C, D, V)
-
+                output[source_name] = (avail_tensor, source_tensor, dt_tensor, C, D, V)
         return output
 
     def get_data_filepath(self, season, basin, sid, time, source_name):

multi_sources/models/vit.py
@@ -28,7 +28,10 @@ def restore_dots_in_keys(d):
 class MultiSourceVIT(nn.Module):
     """Uses a ViT backbone to reconstruct images from multiple sources, with a subset
     of them being masked out.
-    Specifically, the model takes inputs as a dict {source_name: (S, DT, C, V)} where:
+    Specifically, the model takes inputs a Mapping from source name
+    to a tuple (A, S, DT, C, V) where:
+    - A is a tensor of shape (batch_size, 1) whose value is 1 if the source is present,
+        0 if it is missing, and -1 if it is masked.
     - S is a tensor of shape (batch_size) containing the source indices.
     - DT is a tensor of shape (batch_size) containing the time delta.
     - C is a tensor of shape (batch_size, 3, H, W) containing the (lat, lon, land_mask) info
@@ -65,7 +68,8 @@ class MultiSourceVIT(nn.Module):
             dim_head (int): Dimension of each head.
             output_resnet_depth (int): Depth of the output ResNet.
                 set to 0 to disable any convolutional layer at the output.
-            output_resnet_inner_channels (int): Number of channels in the inner layers of the output
+            output_resnet_inner_channels (int): Number of channels in the inner layers
+                of the output
             output_resnet_kernel_size (int): Kernel size of the output ResNet.
             dropout (float): Dropout rate.
             emb_dropout (float): Dropout rate for the embeddings.
@@ -160,15 +164,7 @@ class MultiSourceVIT(nn.Module):
     def forward(self, inputs):
         """
         Args:
-            inputs (dict of str to tensor): Inputs for each source, as a dict
-                {source_name: (S, DT, C, V)} where:
-                - S is a tensor of shape (batch_size) containing the source indices.
-                - DT is a tensor of shape (batch_size) containing the time delta.
-                - C is a tensor of shape (batch_size, 3, H, W) containing the (lat, lon, land_mask)
-                    info at each pixel.
-                - V is a tensor of shape (batch_size, K, H, W) containing the values to be
-                    reconstructed.
-                For one or multiple sources, DT, C and V can be masked out.
+            inputs (dict of str to tuple of tensors): Inputs to the model.
         Returns:
             output (dict of str to tensor): Reconstructed values for each source, as a dict
                 {source_name: tensor of shape (batch_size, K, H, W)}.
@@ -178,7 +174,7 @@ class MultiSourceVIT(nn.Module):
         # Also pad the coordinates tensor C to the same size.
         padded_values, padded_coords = {}, {}
         for source_name, (H, W) in self.img_sizes.items():
-            S, DT, C, V = inputs[source_name]
+            A, S, DT, C, D, V = inputs[source_name]
             padded_values[source_name] = F.pad(V, (0, W - V.shape[-1], 0, H - V.shape[-2]))
             padded_coords[source_name] = F.pad(C, (0, W - C.shape[-1], 0, H - C.shape[-2]))
         # For each source, compute the patch embeddings.
@@ -234,7 +230,7 @@ class MultiSourceVIT(nn.Module):
             source_name: torch.stack([s, dt], dim=-1)
             .unsqueeze(1)
             .expand(-1, self.num_patches[source_name], -1)
-            for source_name, (s, dt, _, _) in inputs.items()
+            for source_name, (_, s, dt, _, _, _) in inputs.items()
         }
         # Concatenate the embeddings with the coordinates and the time delta and source indices.
         embeddings = {

multi_sources/models/vit_classes.py
@@ -116,6 +116,7 @@ class ResNet(nn.Module):
             kernel_size (int): Size of the convolutional kernel.
         """
         super().__init__()
+        self.input_channels = channels
         self.layers = nn.ModuleList([])
         for i in range(n_blocks):
             self.layers.append(
@@ -132,7 +133,7 @@ class ResNet(nn.Module):
             channels = inner_channels
         # Create an output convolutional layer for the regression task
         self.output_layer = nn.Conv2d(
-            inner_channels, channels, kernel_size, padding=kernel_size // 2
+            inner_channels, self.input_channels, kernel_size, padding=kernel_size // 2
         )
 
     def forward(self, x):

multi_sources/structure/mae.py
@@ -9,16 +9,14 @@ from multi_sources.utils.scheduler import CosineAnnealingWarmupRestarts
 class MultisourceMaskedAutoencoder(pl.LightningModule):
     """Given a torch model which receives inputs from multiple sources, this class
     wraps it as a masked autoencoder, using Lightning.
-    The structure receives inputs under the form of map {source_name: (S, DT, C, D, V)}, where:
-    - S is a tensor of shape (batch_size,) containing the source index.
-    - DT is a tensor of shape (batch_size,) containing the time delta between the element's time
-        and the reference time.
-    - C is a tensor of shape (batch_size, 3, H, W) containing the coordinates
-        (lat, lon) of each pixel,
-        and the land mask.
-    - D is a tensor of shape (batch_size, H, W) containing the distance to the
-        storm center at each pixel.
-    - V is a tensor of shape (batch_size, channels, H, W) containing the values of each pixel.
+    The structure receives inputs as a map {source_name: (A, S, DT, C, D, V)}, where:
+    - A is a scalar tensor of shape (1,) containing 1 if the element is available and 0 otherwise.
+    - S is a scalar tensor of shape (1,) containing the index of the source.
+    - DT is a scalar tensor of shape (1,) containing the time delta between the synoptic time
+      and the element's time, normalized by dt_max.
+    - C is a tensor of shape (3, H, W) containing the latitude, longitude, and land-sea mask.
+    - D is a tensor of shape (H, W) containing the distance to the center of the storm.
+    - V is a tensor of shape (n_variables, H, W) containing the variables for the source.
     The model is expected to receive the same input map, and return a map {source_name: V'}
     where V' are the reconstructed values of the input.
 
@@ -44,44 +42,43 @@ class MultisourceMaskedAutoencoder(pl.LightningModule):
         self.enable_masking = enable_masking
         self.save_hyperparameters()
 
-    def loss_fn(self, y_pred, y_true, masked_source_name):
+    def loss_fn(self, y_pred, y_true):
         """Computes the reconstruction loss over the masked source.
         The reconstruction loss is computed as the average over all pixels of:
           (ŷ - y)^2 * 1_{dist_to_center(pixel) < R} * (1 - land_mask(pixel)),
         where ŷ is the predicted value, y is the true value, and R is an arbitrary radius.
         Currently, R is chosen as 1100 km, which is enough to cover the largest storm
         ever recorded (Typhoon Tip, 1979, 2170 km diameter).
-        The land pixels are not considered in the loss, as the model would just learn to
-        predict it instead of the storm.
 
         Args:
             y_pred (dict): The predicted values. See the class description for the expected format.
             y_true (dict): The true values.
-            masked_source_name (str): The name of the source that was masked (such
-                that the task is to reconstruct this source only).
 
         Returns:
             torch.Tensor: The loss of the model.
         """
-        _, _, c, d, v = y_true[masked_source_name]
-        # Compute the MSE loss element-wise
-        loss = (y_pred[masked_source_name] - v) ** 2
-        # The land mask is the third channel of the coordinates tensor
-        land_mask = c[:, 2]
-        # Mask the loss for land pixels
-        loss = loss * (1 - land_mask.unsqueeze(1))
-        # Mask the loss for pixels outside the storm radius
-        loss = loss * (d.unsqueeze(1) < 1100)
-        return loss.mean()
+        losses = {}
+        for source_name, (a, _, _, _, d, v) in y_true.items():
+            pred = y_pred[source_name]
+            # Compute the MSE loss element-wise
+            loss = (pred - v) ** 2
+            # Mask the loss for pixels outside the storm radius
+            loss = loss * (d.unsqueeze(1) < 1100)
+            # If the source is not available, don't include it in the loss
+            # computation
+            loss[a == 0] = 0
+            losses[source_name] = loss.mean()
+        # Return the sum of the losses
+        return sum(losses.values())
 
     def step(self, batch, batch_idx, train_or_val):
         """Defines a training or validation step for the model."""
         # Preprocess the input
         batch = self.preproc_input(batch)
         # Forward pass
-        pred, masked_source_name = self.forward(batch)
+        pred = self(batch)
         # Compute and log the loss
-        loss = self.loss_fn(pred, batch, masked_source_name)
+        loss = self.loss_fn(pred, batch)
         self.log(f"{train_or_val}_loss", loss, prog_bar=True)
         # Compute metrics
         for metric_name, metric_fn in self.metrics.items():
@@ -115,18 +112,16 @@ class MultisourceMaskedAutoencoder(pl.LightningModule):
 
     def predict_step(self, batch, batch_idx):
         """Defines a prediction step for the model."""
-        # Preprocess the input
         batch = self.preproc_input(batch)
-        return self.forward(batch)[0]
+        return self(batch)[0]
 
     def preproc_input(self, x):
         """Fills NaN values in the input tensors with zeros, and normalizes the coordinates."""
-        # x is a map {source_name: S, DT, C, D, V}
+        # x is a map {source_name: A, S, DT, C, D, V}
         # - Fill NaN values (masked / missing) in DT, C and V with zeros
         #   (which is also the mean value of the data after normalization)
-        # - Remove D from the input, as it shouldn't be accessible to the model
         input_ = {}
-        for source, (s, dt, c, d, v) in x.items():
+        for source, (a, s, dt, c, d, v) in x.items():
             # Don't modify the tensors in-place, as we need to keep the NaN values
             # for the loss computation
             dt = torch.nan_to_num(dt, nan=0)
@@ -138,48 +133,48 @@ class MultisourceMaskedAutoencoder(pl.LightningModule):
             # Note: the lon is in the range [0, 360]
             c[:, 0] = c[:, 0] / 90
             c[:, 1] = (c[:, 1] - 180) / 180
-            input_[source] = (s, dt, c, d, v)
+            input_[source] = (a, s, dt, c, d, v)
         return input_
 
     def forward(self, batch):
         """Computes the forward pass of the model"""
         # Mask the input
-        masked_batch, masked_source_name = self.mask(batch)
-        # Remove the "distance to center" tensor from the input
-        input_ = {source: (s, dt, c, v) for source, (s, dt, c, _, v) in masked_batch.items()}
-        return self.model(input_), masked_source_name
+        masked_batch = self.mask(batch)
+        return self.model(masked_batch)
 
     def mask(self, x):
         """Given a training batch, randomly selects a source and masks all of its values.
 
         Args:
-            x (dict): The input batch. See the class description for the expected format.
+            x (dict of str to tuple of tensors): The input batch.
 
         Returns:
-            masked_x (dict): The masked input batch.
-            masked_source_name (str): The name of the source that was masked.
+            masked_x (dict of str to tuple of tensors): The masked input batch.
         """
-        # Randomly select a source to mask, until finding one such that
-        # dt is not fully nan.
-        source_name = None
-        all_sources = list(x.keys())
-        permuted_sources = [all_sources[i] for i in torch.randperm(len(all_sources))]
-        for source in permuted_sources:
-            _, dt, _, _, _ = x[source]
-            if not torch.isnan(dt).all():
-                source_name = source
-                break
-        if source_name is None:
-            raise ValueError("All sources are missing.")
-        if not self.enable_masking:
-            return x, source_name
+        # For each element in the batch, we need to select one source to mask.
+        # However, we can't mask a source that is not available, as there is no
+        # available target to compare the reconstruction with.
+        # Small trick to do so:
+        # - For each element, for each source, generate a random number between 0 and 1.
+        # - If the source is not available, set the random number to -1.
+        # - The source to mask is the one with the highest random number.
+        # - If all sources are not available (random number is -1), raise an error.
+        source_names = list(x.keys())
+        batch_size = x[source_names[0]][0].shape[0]
+        random_numbers = torch.rand(batch_size, len(source_names))
+        for i, source_name in enumerate(source_names):
+            a = x[source_name]  # batch of availability tensors of shape (batch_size,)
+            random_numbers[a == 0, i] = -1
+        values, indices = torch.max(random_numbers, dim=1)
+        if values.min().item() == -1:
+            raise ValueError("Found an element for which all sources are missing.")
+        # Deduce which source to mask for each elemente
         masked_x = {}
-        for source, (s, dt, c, d, v) in x.items():
-            if source == source_name:
-                # Mask the values
-                v = torch.zeros_like(v).to(v.dtype)
-            masked_x[source] = (s, dt, c, d, v)
-        return masked_x, source_name
+        for i, (a, s, dt, c, d, v) in x.items():
+            masked_v = v.clone()
+            masked_v[indices == i] = 0
+            masked_x[i] = (a, s, dt, c, d, masked_v)
+        return masked_x
 
     def configure_optimizers(self):
         """Configures the optimizer for the model.
@@ -193,6 +188,3 @@ class MultisourceMaskedAutoencoder(pl.LightningModule):
             **self.lr_scheduler_kwargs,
         )
         return [optimizer], [scheduler]
-
-    def __call__(self, x):
-        return self.forward(x)