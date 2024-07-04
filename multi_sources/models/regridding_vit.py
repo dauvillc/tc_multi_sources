Simplified and fixed the coords normalization

multi_sources/models/regridding_vit.py
@@ -116,16 +116,13 @@ class RegriddingTransformer(nn.Module):
             output (dict of str to tensor): Reconstructed values for each source, as a dict
                 {source_name: tensor of shape (batch_size, channels, H, W)}.
         """
-        images, coords, contexts, missing_values = [], [], [], []
+        images, coords, contexts = [], [], []
         for source_name, (A, S, DT, C, D, V) in inputs.items():
             images.append(V)
             coords.append(C[:, :2])  # 0 and 1 are lat/lon
             contexts.append(torch.stack([A, S, DT], dim=1))
-            # Missing values have been filled beforehand, but they can be located via
-            # infinite values in D.
-            missing_values.append(D == float("inf"))
         # Normalize the coordinates to be in the range [-1, 1].
-        coords = normalize_coords_across_sources(coords, ignore_mask=missing_values)
+        coords = normalize_coords_across_sources(coords)
         # Store the original image sizes.
         original_img_sizes = [image.shape[-2:] for image in images]
         if self.downsample:

multi_sources/models/utils.py
@@ -31,38 +31,22 @@ def pad_to_next_multiple_of(tensor, multiple_of, **kwargs):
     return F.pad(tensor, padding, **kwargs)
 
 
-def normalize_coords_across_sources(coords, ignore_mask=None):
+def normalize_coords_across_sources(coords):
     """Normalizes the coordinates across multiple sources, by setting
     their min to 0 and their max to 1.
     Args:
         coords (list of torch.Tensor): list of tensors of shape (B, 2, H, W).
-        ignore_mask (list of torch.Tensor): list of boolean tensor of shape (B, H, W),
-            where True indicates that the coordinates at that pixel should be ignored
-            during normalization.
     Returns:
         list of torch.Tensor: the normalized coordinates.
     """
     shapes = [c.shape for c in coords]
     coords = [c.view(c.shape[0], 2, -1) for c in coords]
-    if ignore_mask is None:
-        ignore_mask = [torch.zeros_like(c[:, 0, :]).bool() for c in coords]
-    else:
-        ignored_mask = [m.view(m.shape[0], 1, -1).expand(-1, 2, -1) for m in ignore_mask]
-    min_vals = [
-        c.masked_fill(mask, float("inf")).min(dim=-1)[0] for c, mask in zip(coords, ignored_mask)
-    ]
-    max_vals = [
-        c.masked_fill(mask, -float("inf")).max(dim=-1)[0] for c, mask in zip(coords, ignored_mask)
-    ]
-    # Compute the min and max over each channel (lat and lon)
-    # Compute the min and max over all sources
-    min_vals = torch.stack(min_vals, dim=0).min(dim=0)[0]  # (B, 2)
-    max_vals = torch.stack(max_vals, dim=0).max(dim=0)[0]
-    # Normalize the coordinates
-    norm_coords = [
-        (c.clone() - min_vals[:, :, None]) / (max_vals - min_vals)[:, :, None] for c in coords
-    ]
-    # Where the mask is True, set the normalized coordinates to the original ones
-    for norm_c, c, mask in zip(norm_coords, coords, ignored_mask):
-        norm_c[mask] = c[mask]
-    return [c.view(s) for c, s in zip(norm_coords, shapes)]
+    # Compute the min and max values of the latitudes and longitudes.
+    min_vals = [c.min(dim=-1, keepdim=True)[0] for c in coords]
+    max_vals = [c.max(dim=-1, keepdim=True)[0] for c in coords]
+    # Normalize the latitudes and longitudes.
+    coords = [(c - min_vals[i]) / (max_vals[i] - min_vals[i])
+              for i, c in enumerate(coords)]
+    # Reshape the coordinates.
+    coords = [c.view(*s) for c, s in zip(coords, shapes)]
+    return coords