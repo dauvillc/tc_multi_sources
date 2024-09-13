2353e987649b72a1d647d97aeb5a725f03d977fa
Clément Dauvilliers <clement.dauvilliers@inria.fr>
2024-09-13 20:16:15 +0200

    Fixed a bug

--- multi_sources/models/general_backbone.py
+++ multi_sources/models/general_backbone.py
@@ -107,6 +107,7 @@ class MultisourceGeneralBackbone(nn.Module):
                 # Apply the layer and add the skip connection
                 new_values = layer(new_x, attention_mask=attention_mask)  # dict {source_name: tensor}
                 for source_name, data in new_x.items():
-                    new_x["embedded_values"] = new_x["embedded_values"] + new_values[source_name]
+                    new_val = new_values[source_name] + new_x[source_name]["embedded_values"]
+                    new_x[source_name]["embedded_values"] = new_val
                 x = new_x
         return {source_name: x[source_name]['embedded_values'] for source_name in x.keys()}
